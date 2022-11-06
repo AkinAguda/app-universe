77b0d90704e966c43b9cd295bdd6ab8182535a19
Akin Aguda <lordtitan7@gmail.com>
2022-11-06 18:24:28 +0100

    changed api to use create_universe and renamed wrapper to AppUniverse and made old AppUniverse to be AppUniverseCore

--- src/lib.rs
+++ src/lib.rs
@@ -85,12 +85,18 @@ use std::sync::{Arc, RwLock, RwLockReadGuard};
 /// Cloning an `AppUniverseWrapper` is a very cheap operation.
 ///
 /// All clones hold pointers to the same inner state.
-pub struct AppUniverseWrapper<W: AppUniverse> {
-    universe: Arc<RwLock<W>>,
+pub struct AppUniverse<U: AppUniverseCore> {
+    universe: Arc<RwLock<U>>,
+}
+
+/// This alloows you to create an app universe
+/// TODO - Better Documentation
+pub fn create_universe<U: AppUniverseCore + 'static>(universe_core: U) -> AppUniverse<U> {
+    AppUniverse::new(universe_core)
 }
 
 /// Defines how messages that indicate that something has happened get sent to the universe.
-pub trait AppUniverse: Sized {
+pub trait AppUniverseCore: Sized {
     /// Indicates that something has happened.
     ///
     /// ```
@@ -108,10 +114,10 @@ pub trait AppUniverse: Sized {
     fn msg(&mut self, message: Self::Message);
 }
 
-impl<U: AppUniverse + 'static> AppUniverseWrapper<U> {
+impl<U: AppUniverseCore + 'static> AppUniverse<U> {
     /// Create a new AppUniverseWrapper.
-    pub fn new(universe: U) -> Self {
-        let universe = Arc::new(RwLock::new(universe));
+    fn new(universe_core: U) -> Self {
+        let universe = Arc::new(RwLock::new(universe_core));
         Self { universe }
     }
 
@@ -143,10 +149,52 @@ impl<U: AppUniverse + 'static> AppUniverseWrapper<U> {
     }
 }
 
-impl<W: AppUniverse> Clone for AppUniverseWrapper<W> {
+impl<W: AppUniverseCore> Clone for AppUniverse<W> {
     fn clone(&self) -> Self {
-        AppUniverseWrapper {
+        AppUniverse {
             universe: self.universe.clone(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestAppState {
+        counter: u8,
+    }
+
+    pub enum Msg {
+        Increment(u8),
+    }
+
+    impl AppUniverseCore for TestAppState {
+        type Message = Msg;
+
+        fn msg(&mut self, message: Self::Message) {
+            match message {
+                Msg::Increment(value) => {
+                    self.counter += value;
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn action_dispatch_works() {
+        let state = TestAppState { counter: 0 };
+        let universe = create_universe(state);
+
+        universe.msg(Msg::Increment(3));
+
+        assert_eq!(universe.read().counter, 3);
+    }
+
+    #[test]
+    fn subscription_works() {
+        let mut some_value_to_update = 100;
+        let state = TestAppState { counter: 0 };
+        let universe = create_universe(state);
     }
 }
