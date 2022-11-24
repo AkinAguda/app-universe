dcb1bf65696914f24341735045cf68d0cc9eaeec
Akin Aguda <lordtitan7@gmail.com>
2022-11-24 21:42:53 +0100

    multiple subs seems to work

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -54,6 +54,38 @@ mod tests {
     }
 
     #[test]
+    fn multiple_subscriptions_work_sync() {
+        use std::cell::RefCell;
+
+        let some_value = Rc::new(RefCell::new(100));
+        let some_value_clone = some_value.clone();
+        let some_value_clone2 = some_value.clone();
+        let state = TestAppState { counter: 0 };
+
+        let mut universe = AppUniverse::new(state);
+
+        let sub1 = universe.subscribe(Box::new(move |universe| {
+            let c = universe.read().counter;
+            *some_value_clone.borrow_mut() += c;
+        }));
+
+        let sub2 = universe.subscribe(Box::new(move |universe| {
+            let g = universe.read().counter;
+            *some_value_clone2.borrow_mut() -= g;
+        }));
+
+        universe.msg(Msg::Increment(1));
+
+        universe.unsubscribe(sub1);
+
+        universe.unsubscribe(sub2);
+
+        universe.msg(Msg::Increment(1));
+
+        assert_eq!(*some_value.borrow(), 100);
+    }
+
+    #[test]
     fn unsubscription_works_sync() {
         use std::cell::RefCell;
 
